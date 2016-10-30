04d8259554940d107601d1d95d5c1f1fa6fcfbfa
1ncontinentia <JE@JabreyBook.lan>
2016-10-30 20:08:59 +0000

    Removed group persistence definitions

--- INCON/INC_undercover/functionsWhitelist.hpp
+++ INCON/INC_undercover/functionsWhitelist.hpp
@@ -1,8 +1,7 @@
 
-class aliveDataHandler {allowedTargets=0;};
 class countAlerted {allowedTargets=0;};
-class exportLoadout {allowedTargets=0;};
 class getFactionGear {allowedTargets=0;};
+class getLoadout {allowedTargets=0;};
 class recruitAttempt {allowedTargets=0;};
 class recruitCiv {allowedTargets=0;};
 class recruitSuccess {allowedTargets=0;};
@@ -18,4 +17,3 @@ class undercoverKilledHandler {allowedTargets=0;};
 class undercoverTrespassHandler {allowedTargets=0;};
 class undercoverTrespassMarkerLoop {allowedTargets=0;};
 class undercoverTrespassRadiusLoop {allowedTargets=0;};
-class unitPersist {allowedTargets=0;};
