d482392b3921348c1de1e0fc7ef9cd05d563154f
1ncontinentia <JE@JabreyBook.lan>
2016-10-31 22:01:47 +0000

    Replaced function with BIS version

--- INCON/INC_groupPersist/cfgFunctions.hpp
+++ INCON/INC_groupPersist/cfgFunctions.hpp
@@ -1,7 +1,6 @@
 class groupPersist
 {
 	file = "INCON\INC_groupPersist\func";
-	class exportLoadout {description = "Copy a unit's loadout and store it in SQF.";};
 	class groupPersist {description = "Main class for group persistence.";};
 	class persHandler {description = "Gets data for unit persistence and executes it."};
 };
